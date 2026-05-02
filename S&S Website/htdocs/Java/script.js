5bbf10c5bad151d260e484c94e7872481d25560b
Jaden Garcia <garc1220@purdue.edu>
2026-05-01 22:34:59 -0400

    Add files via upload

---
+++ S&S Website/htdocs/Java/script.js
@@ -0,0 +1,16 @@
+document.getElementById("brzbtn").addEventListener("click", function () {
+    window.location.href = "services.html";
+});
+
+document.getElementById("showbtn").addEventListener("click", function () {
+    window.location.href = "contact.html";
+});
+
+document.getElementById("infobtn").addEventListener("click", function () {
+    window.location.href = "about.html";
+});
+
+document.getElementById("ques-btn").addEventListener("click", function () {
+    window.location.href = "faq.html";
+});
+
